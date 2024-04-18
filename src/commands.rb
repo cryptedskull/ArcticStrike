b16c37696e0a9b5a085dade5a972df93bd46884d
CryptedSkull <112264028+cryptedskull@users.noreply.github.com>
2024-04-18 11:00:49 -0500

    Namespace restructure

--- src/commands.rb
+++ src/commands.rb
@@ -1,52 +1,53 @@
-module Commands
-    def self.whoami(args)
-        puts "You are the user, duh!"
-    end
-
-    def self.clear()
-        print "\e[2J\e[f"
-    end
-
-    def self.connect(args)
-        if args.length < 3
-            puts "Usage: connect <target_ip> <target_port> <message>"
-            return
-          end
-        Networking.tcp_connect(args[0], args[1].to_i, args[2..].join(' '))
-    end
-
-    def self.listen(args)
-        if args.length < 2
-            puts "Usage: server <listen_ip> <listen_port> [interrupt (true/false)]"
-            return
+module ArcticStrike
+    module Commands
+        def self.whoami(args)
+            puts "You are the user, duh!"
         end
+
+        def self.clear()
+            print "\e[2J\e[f"
+        end
+
+        def self.connect(args)
+            if args.length < 3
+                puts "Usage: connect <target_ip> <target_port> <message>"
+                return
+            end
+            Networking.tcp_connect(args[0], args[1].to_i, args[2..].join(' '))
+        end
+
+        def self.listen(args)
+            if args.length < 2
+                puts "Usage: server <listen_ip> <listen_port> [interrupt (true/false)]"
+                return
+            end
+            
+            Networking.tcp_listen(args[0], args[1].to_i, args[2].to_s)
+        end
+
+
+        def self.exit()
+            Handler.interrupt()
+        end
+
+        def self.banner(banner_file)
+            banner = File.readlines(banner_file).map(&:chomp)
+            terminal_width = `tput cols`.to_i
         
-        Networking.tcp_listen(args[0], args[1].to_i, args[2].to_s)
-    end
-
-
-    def self.exit()
-        Handler.interrupt()
-    end
-
-    def self.banner(banner_file)
-        banner = File.readlines(banner_file).map(&:chomp)
-        terminal_width = `tput cols`.to_i
-      
-        banner.each do |line|
-          padding = (terminal_width - line.length) / 2
-          padding.times { print " " }
-          puts line
+            banner.each do |line|
+            padding = (terminal_width - line.length) / 2
+            padding.times { print " " }
+            puts line
+            end
         end
-      end
-
-end
+    end
 
 
-module Handler
-    def self.interrupt
-        Commands.clear()
-        puts "[!] Now exiting ArcticStrike, goodbye!"
-        exit
+    module Handler
+        def self.interrupt
+            Commands.clear()
+            puts "[!] Now exiting ArcticStrike, goodbye!"
+            exit
+        end
     end
 end
--- src/user_interface/prompt.rb
+++ src/user_interface/prompt.rb
@@ -1,5 +1,5 @@
 def user_prompt
     while buf = Readline.readline("╭─[[ #{@PROGRAM_NAME} ]]─[#{Time.new.strftime("%X")}]\n╰─< #{@ACTIVE_AREA} > :: ", true)
-        CommandParser.input(buf)
+        ArcticStrike::CommandParser.input(buf)
     end
 end
