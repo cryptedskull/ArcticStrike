1a6c6820b4172d7253a0bea493f0059e00616b5b
CryptedSkull <112264028+cryptedskull@users.noreply.github.com>
2024-04-19 21:47:38 -0500

    Improved command parser

--- src/command_parser.rb
+++ src/command_parser.rb
@@ -3,23 +3,12 @@ module CommandParser
     def self.input(buf)
         args = buf.strip.split(/\s+/)
         command = args.shift
-        case command
-        when "whoami"
-            Commands.whoami(args)
-        when "help"
-            Commands.help(args)
-        when "exit" || "quit"
-            Commands.exit()
-        when "connect"
-            Commands.connect(args)
-        when "listen"
-            Commands.listen(args)
-        when "clear"
-            Commands.clear()
-        when "banner"
-            Commands.banner(args)
-        when "easter-egg"
-            puts "Hi :)"
+        command_method = Commands.method(command) if Commands.respond_to?(command)
+
+        if command_method
+          command_method.call(args)
+        elsif command == "exit" || command == "quit"
+          Commands.exit()
         else
             puts "Invalid command: #{command}"
         end
--- src/commands.rb
+++ src/commands.rb
@@ -1,56 +1,63 @@
 module ArcticStrike
-module Commands
-    def self.whoami(args)
-        puts "You are the user, duh!"
-    end
-
-    def self.clear()
-        print "\e[2J\e[f"
-    end
-
-    def self.connect(args)
-        if args.length < 3
-            puts "Usage: connect <target_ip> <target_port> <message>"
-            return
+    module Commands
+        def self.help(args)
+            puts("Sorry, I can't help you yet!")
         end
-        ArcticStrike::Networking::Sockets.tcp_connect(args[0], args[1].to_i, args[2..].join(' '))
-    end
 
-    def self.listen(args)
-        if args.length < 2
-            puts "Usage: server <listen_ip> <listen_port>"
-            return
+        def self.whoami(args)
+            puts "You are the user, duh!"
         end
-        
-        ArcticStrike::Networking::Sockets.tcp_listen(args[0], args[1].to_i)
-    end
 
+        def self.clear(args)
+            print "\e[2J\e[f"
+        end
 
-    def self.exit()
-        Handler.interrupt()
-    end
+        def self.exec(args)
+            system args
+        end
 
-    def self.banner(banner_file = "resources/banner.txt")
-        begin
-            banner = File.readlines(banner_file).map(&:chomp)
-            terminal_width = `tput cols`.to_i
-        
-            banner.each do |line|
-            padding = (terminal_width - line.length) / 2
-            padding.times { print " " }
-            puts line
+        def self.connect(args)
+            if args.length < 3
+                puts "Usage: connect <target_ip> <target_port> <message>"
+                return
             end
-        rescue => e
-            puts "[ERROR]> #{e}"
+            ArcticStrike::Networking::Sockets.tcp_connect(args[0], args[1].to_i, args[2..].join(' '))
+        end
+
+        def self.listen(args)
+            if args.length < 2
+                puts "Usage: server <listen_ip> <listen_port>"
+                return
+            end
+            
+            ArcticStrike::Networking::Sockets.tcp_listen(args[0], args[1].to_i)
+        end
+
+
+        def self.exit(args)
+            Handler.interrupt()
+        end
+
+        def self.banner(banner_file = "resources/banner.txt")
+            begin
+                banner = File.readlines(banner_file).map(&:chomp)
+                terminal_width = `tput cols`.to_i
+            
+                banner.each do |line|
+                padding = (terminal_width - line.length) / 2
+                padding.times { print " " }
+                puts line
+                end
+            rescue => e
+                puts "[ERROR]> #{e}"
+            end
         end
     end
-end
-
-module Handler
-    def self.interrupt
-        Commands.clear()
-        puts "[!] Now exiting ArcticStrike, goodbye!"
-        exit
+    module Handler
+        def self.interrupt
+            Commands.clear(args)
+            puts "[!] Now exiting ArcticStrike, goodbye!"
+            exit
+        end
     end
-end
 end
